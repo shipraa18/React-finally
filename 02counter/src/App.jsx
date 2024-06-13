b08ce9f6b9b84944111e05557ee393e5acddb37f
Shipra <shipramaurya1804@gmail.com>
2024-06-13 23:14:53 +0530

    some changes

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+
+  const [counter,setCounter]=useState(15)
+
+
+  const addValue = ()=>{
+    // counter=counter+1;
+    setCounter(counter+1);
+  }
+
+  const removeValue = () =>{
+
+    setCounter(counter-1);
+
+  }
+
+  return (
+    <>
+    <h1>chai aur react</h1>
+    <h2>counter value: {counter}</h2>
+    <button onClick={addValue}>add value{counter}</button>
+    <br/>
+    <button onClick={removeValue}>remove value{counter}</button>
+
+    <p>footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
+ 
